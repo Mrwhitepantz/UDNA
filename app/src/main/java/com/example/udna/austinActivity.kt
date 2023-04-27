b8d533f64fc4c8f5bdcfb671776d3a0a9dc0dad7
AustinSickles <45471377+AustinSickles@users.noreply.github.com>
2023-04-26 17:24:35 -0700

    test

---
+++ app/src/main/java/com/example/udna/austinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.udna
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class austinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_austin)
+    }
+}
