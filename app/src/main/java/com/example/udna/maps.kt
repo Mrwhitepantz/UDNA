5fc1a8bb406410fbdc0cfe5ce228d5890b0765ff
SoTypical <noah.b.guerrero@gmail.com>
2023-04-26 18:51:00 -0700

    added most activities and connected

---
+++ app/src/main/java/com/example/udna/maps.kt
@@ -0,0 +1,24 @@
+package com.example.udna
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class maps : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val button = findViewById<Button>(R.id.ForestTest)
+        button.setOnClickListener{
+            val intent = Intent(this,map_content::class.java)
+            startActivity(intent)
+        }
+        val buttonback = findViewById<Button>(R.id.back)
+        buttonback.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
